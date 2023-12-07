45b3d8b598a4119fe7187781c405749002fba888
Göksu Eldemir <55974337+eldemir18@users.noreply.github.com>
2023-12-07 09:01:05 +0300

    Add files via upload

---
+++ MaxStack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MaxStack<T> : IEnumerable<T>, IEnumerable, ICollection, IReadOnlyCollection<T> where T : IComparable<T>
+{
+    private readonly Stack<T> _stack;
+    private readonly Stack<T> _maxStack;
+    
+    public T Max => _maxStack.Peek();
+    public int Count => _stack.Count;
+
+    public bool IsSynchronized => ((ICollection)_stack).IsSynchronized;
+    public object SyncRoot => ((ICollection)_stack).SyncRoot;
+
+    public MaxStack()
+    {
+        _stack = new Stack<T>();
+        _maxStack = new Stack<T>();
+    }
+
+    public void Push(T value)
+    {
+        T maxValue = value;
+
+        if(Count != 0)
+        {
+            maxValue = value.CompareTo(Max) > 0 ? value : Max;
+        }
+
+        _stack.Push(value);
+        _maxStack.Push(maxValue);
+    }
+
+    public void Pop()
+    {
+        _stack.Pop();
+        _maxStack.Pop();
+    }
+
+    public T Peek()
+    {
+        return _stack.Peek();
+    }
+
+    public bool Contains(T item)
+    {
+        return _stack.Contains(item);
+    }
+
+    public T[] ToArray()
+    {
+        return _stack.ToArray();
+    }
+
+    public void Clear()
+    {
+        _stack.Clear();
+        _maxStack.Clear();
+    }
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+    {
+        return ((IEnumerable<T>)_stack).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)_stack).GetEnumerator();
+    }
+
+    void ICollection.CopyTo(Array array, int index)
+    {
+        ((ICollection)_stack).CopyTo(array, index);
+    }
+}
+
---
+++ MinStack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MinStack<T> : IEnumerable<T>, IEnumerable, ICollection, IReadOnlyCollection<T> where T : IComparable<T>
+{
+    private readonly Stack<T> _stack;
+    private readonly Stack<T> _minStack;
+    
+    public T Min => _minStack.Peek();
+    public int Count => _stack.Count;
+
+    public bool IsSynchronized => ((ICollection)_stack).IsSynchronized;
+    public object SyncRoot => ((ICollection)_stack).SyncRoot;
+    
+    public MinStack()
+    {
+        _stack = new Stack<T>();
+        _minStack = new Stack<T>();
+    }
+
+    public void Push(T value)
+    {
+        T minValue = value;
+
+        if(Count != 0)
+        {
+            minValue = value.CompareTo(Min) < 0 ? value : Min;
+        }
+
+        _minStack.Push(minValue);
+        _stack.Push(value);
+    }
+
+    public void Pop()
+    {
+        _stack.Pop();
+        _minStack.Pop();
+    }
+
+    public T Peek()
+    {
+        return _stack.Peek();
+    }
+
+    public bool Contains(T item)
+    {
+        return _stack.Contains(item);
+    }
+
+    public T[] ToArray()
+    {
+        return _stack.ToArray();
+    }
+
+    public void Clear()
+    {
+        _stack.Clear();
+        _minStack.Clear();
+    }
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+    {
+        return ((IEnumerable<T>)_stack).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)_stack).GetEnumerator();
+    }
+
+    void ICollection.CopyTo(Array array, int index)
+    {
+        ((ICollection)_stack).CopyTo(array, index);
+    }
+}
+
